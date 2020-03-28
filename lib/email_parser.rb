fbe98a4940fd2689d2acf4d222df15588fc70a04
Adam Priest <sepone229@gmail.com>
2020-03-28 13:02:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
   end
   
   def parse
-    @csv_data.split.collect do |address|
+    csv_data.split.collect do |address|
       address.split(',')
     end
     .flatten.uniq
